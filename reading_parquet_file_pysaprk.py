619008b04b4bcef58eb8bc465887a3b3655771cf
Kashyap <krathore911@gmail.com>
2021-10-26 12:41:58 +0530

    file update

--- reading_parquet_file_pysaprk.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-path = ""
-file_name = ""
-parquet_file = path + file_name
-
-spark_session = SparkSession.builder.appName("ParquestFileSession").getOrCreate()
-par_dataF = spark_session.read.parquet(parquet_file)
-par_dataF.show()
